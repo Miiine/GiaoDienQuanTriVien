41bac71754ba32cb7a3c85fb2a4ef4f406ddec5f
Baokhue312 <106755789+Baokhue312@users.noreply.github.com>
2023-10-26 05:45:24 +0700

    qlnd

---
+++ automotive/src/main/java/com/example/giaodienquantrivien/Quanlytaikhoan.java
@@ -0,0 +1,14 @@
+package com.example.giaodienquantrivien;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Quanlytaikhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quanlytaikhoan);
+    }
+}
